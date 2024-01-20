f8a2b3592d2234c8358436198e6d9b44fbb4d6ec
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2024-01-20 08:51:29 -0400

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use axum::routing::{get, Router};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     let router = Router::new().route("/", get(greet));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
     tracing::info!("listening on {}", listener.local_addr().unwrap());
