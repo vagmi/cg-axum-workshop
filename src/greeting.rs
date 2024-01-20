c9aaaa3652dbc06aad68651d91ad444cfe1e5a21
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2024-01-20 09:02:57 -0400

    Add path param

--- src/greeting.rs
+++ src/greeting.rs
@@ -1,14 +1,19 @@
-pub async fn greet() -> String { 
-    "hello world".to_string() 
+use axum::extract::Path;
+
+pub async fn greet(Path(name): Path<String>) -> String { 
+    format!("hello {}", name)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use axum::extract::Path;
 
     #[tokio::test]
     async fn test_greet() {
-        assert_eq!(greet().await, "hello world");
+        assert_eq!(
+            greet(Path("chennai geeks".to_string())).await, 
+            "hello chennai geeks");
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::routing::{get, Router};
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let router = Router::new().route("/", get(greet));
+    let router = Router::new().route("/:name", get(greet));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
     tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, router.into_make_service()).await.unwrap();
